1499e8b53cc1982669b5f1138da6ecb12a8a207c
Saqib <saqim9493@gmail.com>
2020-11-23 13:19:15 +0500

    initial commit

--- app/src/main/java/com/example/lawyerassistance/Dashboard.java
+++ app/src/main/java/com/example/lawyerassistance/Dashboard.java
@@ -1,11 +1,10 @@
 package com.example.lawyerassistance;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Dashboard extends AppCompatActivity {
 Button profile,lawyers,laws,recommend,case_study;
